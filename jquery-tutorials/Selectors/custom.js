edee9f7b8ceef7795c88baa1d2f09189cdfe606b
Pradip Basel <pradipbaselatwork@gmail.com>
2026-04-28 23:29:33 +0545

    jquery selectors tags,id,class.

---
+++ jquery-tutorials/Selectors/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    //     $("#heading1").hide();
+    // //    $(".heading2").hide();
+    //    $("#para1").hide();
+
+    //    $("span").css('color','green');
+
+    $("h1#heading1:first").css('color', 'red')
+
+    // $(":button").hide();
+    // $(":submit").hide();
+    // $(":text").hide();
+
+    // $('[href]').css('color','orange');
+    $("a[href='https://www.youtube.com']").css('color', 'red');
+
+});
